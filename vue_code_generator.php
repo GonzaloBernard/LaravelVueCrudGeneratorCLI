86a5b8fea05b6d70846d9627ea65172bd126f4a4
GonzaloBernard <gonza.bernard.sf@gmail.com>
2022-09-23 14:50:06 -0300

    Getting into vue

--- vue_code_generator.php
+++ vue_code_generator.php
@@ -11,7 +11,8 @@ $prefix = ''; // Root folder
 /* $output_model_prefix = $output_controller_prefix = $output_store_request_prefix = 
         $output_update_request_prefix = $output_resource_prefix = $output_migration_prefix = 'vendor/generate/dist/'; */
 $api_path = 'routes/api.php';
-$output_single = 'store/cruds/';
+$output_create = 'cruds/';
+$output_index = $output_single = 'store/cruds/';
 $output_controller_prefix = 'app/Http/Controllers/Api/V1/Admin/';
 $output_store_request_prefix = 'app/Http/Requests/';
 $output_update_request_prefix = 'app/Http/Requests/';
@@ -78,25 +79,47 @@ echo "\n-------------------------------------------------\n";
 // Add views
 // cruds /{NewEntity}/{Index.vue Show.vue Create.vue Edit.vue}
 /////////////////
+mkdir(
+    "cruds/${entity_name}",
+    $recursive = true,
+);
+$filetext = readTemplate('create.txt',$prefix , $entity_name, $name_camelcase, $db_name);
+echo "-$filetext-\n";
+
+$create_attributes = "";
+
+if(strlen($string_attributes[0]) > 1) {
+    foreach ($string_attributes as $attribute) {
+        $create_attributes .= '$table->string(' . "'$attribute'); \n\t\t\t";
+    }
+}
+if(strlen($integer_attributes[0]) > 1) {
+    foreach ($integer_attributes as $attribute) {
+        $create_attributes .= '$table->integer(' . "'$attribute'); \n\t\t\t";
+    }
+}
+if(strlen($float_attributes[0]) > 1) {
+    foreach ($float_attributes as $attribute) {
+        $create_attributes .= '$table->float(' . "'$attribute'); \n\t\t\t";
+    }
+}
+
+$filetext = str_replace("// create", $create_attributes, $filetext);
+$create = fopen("${output_create}${entity_name}/Create.vue", "w");
+fwrite($create, $filetext);
+fclose( $create );
+
 
 // Add to vuex store
 // store /cruds /{NewEntity}/{index.js single.vue}
 /////////////////
-$filename = "${prefix}${entity_name}/single.txt";
-$file = fopen( $filename, "r" );
+mkdir(
+    "store/cruds/${entity_name}",
+    $recursive = true,
+);
+$filetext = readTemplate('single.txt',$prefix , $entity_name, $name_camelcase, $db_name);
+//echo "-$filetext-\n";
 
-if( $file == false ) {
-    echo ( "Error in opening file" );
-    exit();
-}
-
-$filesize = filesize( $filename );
-$filetext = fread( $file, $filesize );
-fclose( $file );
-
-$filetext = str_replace("entity_name", $entity_name, $filetext);
-$filetext = str_replace("name_camelcase", $name_camelcase, $filetext);
-$filetext = str_replace("db_name", $db_name, $filetext);
 $single_attributes = "";
 
 if(strlen($string_attributes[0]) > 1) {
@@ -116,9 +139,35 @@ if(strlen($float_attributes[0]) > 1) {
 }
 
 $filetext = str_replace("// SINGLE", $single_attributes, $filetext);
-$single = fopen("${output_single}single.js", "w");
+$single = fopen("${output_single}${entity_name}/single.js", "w");
 fwrite($single, $filetext);
 fclose( $single );
+
+$filetext = readTemplate('index.txt',$prefix , $entity_name, $name_camelcase, $db_name);
+//echo "-$filetext-\n";
+
+$index_attributes = "";
+
+if(strlen($string_attributes[0]) > 1) {
+    foreach ($string_attributes as $attribute) {
+        $index_attributes .= '$table->string(' . "'$attribute'); \n\t\t\t";
+    }
+}
+if(strlen($integer_attributes[0]) > 1) {
+    foreach ($integer_attributes as $attribute) {
+        $index_attributes .= '$table->integer(' . "'$attribute'); \n\t\t\t";
+    }
+}
+if(strlen($float_attributes[0]) > 1) {
+    foreach ($float_attributes as $attribute) {
+        $index_attributes .= '$table->float(' . "'$attribute'); \n\t\t\t";
+    }
+}
+
+$filetext = str_replace("// index", $index_attributes, $filetext);
+$index = fopen("${output_index}${entity_name}/index.js", "w");
+fwrite($index, $filetext);
+fclose( $index );
 
 
 
@@ -126,4 +175,28 @@ echo "-------------------------------------------------\n";
 echo "-- Entity created !!!! \n";
 echo "-- apparently \n";
 echo "-------------------------------------------------\n";
+
+
+/////////////////////////////////////////////////////
+// FUNCTIONS
+/////////////////////////////////////////////////////
+function readTemplate($template, $prefix , $entity_name, $name_camelcase, $db_name){
+    $filename = "${prefix}templates/${template}";
+    $file = fopen( $filename, "r" );
+
+    if( $file == false ) {
+        echo ( "Error in opening file" );
+        exit();
+    }
+
+    $filesize = filesize( $filename );
+    $filetext = fread( $file, $filesize );
+    fclose( $file );
+
+    $filetext = str_replace("entity_name", $entity_name, $filetext);
+    $filetext = str_replace("name_camelcase", $name_camelcase, $filetext);
+    $filetext = str_replace("db_name", $db_name, $filetext);
+    return $filetext;
+}
+
 ?>
